519f9c793fb6074a4f7a8ac2877b75985b710519
ShomanDanyloIP-12 <shomdan2004@gmail.com>
2026-04-28 02:13:27 +0300

    Update zendesk-highlighter.user.js
    G2 added

--- zendesk-highlighter.user.js
+++ zendesk-highlighter.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Zendesk Highlighter (Safe Mode + Subject)
 // @namespace    http://tampermonkey.net/
-// @version      6.29
+// @version      6.30
 // @description  Highlight key phrases in comments and ticket subject securely without breaking HTML
 // @match        https://*.zendesk.com/*
 // @grant        none
@@ -363,7 +363,8 @@
         "complaints to paypal",
         "Paypal involved",
         "problem to PayPal",
-        "opening a refund file"
+        "opening a refund file",
+        "report to PayPal"
     ];
 
     const DATA_DELETION_TRIGGERS = [
