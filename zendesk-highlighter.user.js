5f80e2db2a53a0102b261e4759ff73336b91db8c
ShomanDanyloIP-12 <shomdan2004@gmail.com>
2026-05-12 02:32:50 +0300

    Update zendesk-highlighter.user.js
    dd added

--- zendesk-highlighter.user.js
+++ zendesk-highlighter.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Zendesk Highlighter (Safe Mode + Subject)
 // @namespace    http://tampermonkey.net/
-// @version      6.65
+// @version      6.66
 // @description  Highlight key phrases in comments and ticket subject securely without breaking HTML
 // @match        https://*.zendesk.com/*
 // @grant        none
@@ -523,7 +523,8 @@
         "remoge my card",
         "cancellation of any registration",
         "cancellation of my subscription/account",
-        "cancel any cards"
+        "cancel any cards",
+        "disassociation"
     ];
 
     const POLICIES_TRIGGERS = [
