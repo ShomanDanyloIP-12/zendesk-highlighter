76e7168b409ae0fe93b69c947b0a1e42b4c948de
ShomanDanyloIP-12 <shomdan2004@gmail.com>
2026-05-12 23:08:44 +0300

    Update zendesk-highlighter.user.js
    Policy word added

--- zendesk-highlighter.user.js
+++ zendesk-highlighter.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Zendesk Highlighter (Safe Mode + Subject)
 // @namespace    http://tampermonkey.net/
-// @version      6.70
+// @version      6.71
 // @description  Highlight key phrases in comments and ticket subject securely without breaking HTML
 // @match        https://*.zendesk.com/*
 // @grant        none
@@ -579,7 +579,8 @@
         "withdrawal period",
         "legal deadline",
         "withdrawing from my contract",
-        "cooling off"
+        "cooling off",
+        "legal right"
     ];
 
     const IGNORED_OTHER_EMAILS = new Set([
