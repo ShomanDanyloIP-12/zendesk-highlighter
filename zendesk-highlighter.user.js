05edeb61ac7e417a8d322421b15c139039c6dded
ShomanDanyloIP-12 <shomdan2004@gmail.com>
2026-05-20 20:27:18 +0300

    Update zendesk-highlighter.user.js
    dd added

--- zendesk-highlighter.user.js
+++ zendesk-highlighter.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Zendesk Highlighter (Safe Mode + Subject)
 // @namespace    http://tampermonkey.net/
-// @version      7.05
+// @version      7.06
 // @description  Highlight key phrases in comments and ticket subject securely without breaking HTML
 // @match        https://*.zendesk.com/*
 // @grant        none
