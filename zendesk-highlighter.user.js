104eec4bdf33299acf580e3188d7d487e1382735
ShomanDanyloIP-12 <shomdan2004@gmail.com>
2026-07-05 22:48:54 +0300

    Update zendesk-highlighter.user.js
    PW added

--- zendesk-highlighter.user.js
+++ zendesk-highlighter.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Zendesk Highlighter (Safe Mode + Subject)
 // @namespace    http://tampermonkey.net/
-// @version      7.77
+// @version      7.78
 // @description  Highlight key phrases in comments and ticket subject securely without breaking HTML
 // @match        https://*.zendesk.com/*
 // @grant        none
@@ -686,7 +686,8 @@
         "of 14 days",
         "14 days to get the refund",
         "withdrawal letter",
-        "right to withdrawal"
+        "right to withdrawal",
+        "withdrawel from"
     ];
 
     const IGNORED_OTHER_EMAILS = new Set([
